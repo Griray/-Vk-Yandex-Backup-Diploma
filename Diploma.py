3c735070b59a88a93bc163805733930185cbeb50
Grigorii Raikov <griray090494@gmail.com>
2020-07-17 23:06:07 +0300

    Adding json file method

--- Diploma.py
+++ Diploma.py
@@ -2,11 +2,8 @@ import requests
 import json
 from pprint import pprint
 
-
 TOKEN = input("Введите токен ")
 
-
-# noinspection PyUnreachableCode
 class User:
     def __init__(self, id):
         self.id = id
@@ -47,10 +44,29 @@ class User:
         return link_list
 
     # Создаю словарь где keys = имя фото, values = ссылка на фото
-    def dict_name_link_photo (self):
+    def dict_name_link_photo(self):
         photo_dict = dict(zip(self.photo_name(), self.photo_link()))
         return photo_dict
 
+    def size_info(self):
+        size_list = []
+        for size in self.user_photos():
+            type = size["sizes"][-1]["type"]
+            size_list.append(type)
+        return size_list
+
+    def preparing_for_json(self):
+        dict_name_size = dict(zip(self.photo_name(), self.size_info()))
+        list_json = []
+        for names, size in dict_name_size.items():
+            dict_json = {"file_name": names, "size": size}
+            list_json.append(dict_json)
+        with open("photo.json", "w") as file:
+            json.dump(list_json, file, indent=2)
+        print("Файл JSON с информацией по фотографиям успешно создан")
+
+
+# 958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008
 mikhail = User(id = 552934290)
 
 print("Все фото пользователя", mikhail.user_photos())
@@ -60,4 +76,9 @@ print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
 print("Ссылки на фотографии пользователя", mikhail.photo_link())
 print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
 print("Словарь названий и ссылок на фотографии", mikhail.dict_name_link_photo())
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
+print("Тип размеров фотографий", mikhail.size_info())
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
+mikhail.preparing_for_json()
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
 
