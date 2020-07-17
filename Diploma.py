843b3deda18270b3e3379543dd393f3ef599bd01
Grigorii Raikov <griray090494@gmail.com>
2020-07-17 13:25:26 +0300

    Code correction, separate 1 method on 4 methods. Delete global variables

--- Diploma.py
+++ Diploma.py
@@ -3,13 +3,14 @@ import json
 from pprint import pprint
 
 
-TOKEN = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
+TOKEN = input("Введите токен ")
 
+
+# noinspection PyUnreachableCode
 class User:
     def __init__(self, id):
         self.id = id
 
-    @property
     def user_photos(self):
         response = requests.get(
             "https://api.vk.com/method/photos.get",
@@ -24,28 +25,39 @@ class User:
             }
         )
         photo_json = response.json()
-        # Создаю 2 списка один с именами фото, другой с сылками на загрузку фото
-        name = []
-        link_list = []
+        return photo_json["response"]["items"]
 
-        # Название для фото делаю сразу состоящее из лайков и даты (не хотелось названий разных форматов)
-        for elements in photo_json["response"]["items"]:
+        # Создаю список с именами фото
+    def photo_name (self):
+        name = []
+        # Название для фото делаю сразу состоящее из лайков и даты (название разных форматов напрягает)
+        for elements in self.user_photos():
             likes = (str(elements["likes"]["count"]))
             date = (str(elements["date"]))
             nick = likes+date
             name.append(nick)
+        return name
 
-        # Ссылки на фото беру последние, они самые большие, поэтому использую "-1" чтобы брать ссылку с конца
-        for reference in photo_json["response"]["items"]:
+    # Ссылки на фото беру последние, они самые большие, поэтому использую "-1" чтобы брать ссылку с конца
+    def photo_link(self):
+        link_list = []
+        for reference in self.user_photos():
             link = reference["sizes"][-1]["url"]
             link_list.append(link)
+        return link_list
 
-        # Создаю словарь где keys = имя фото, values = ссылка на фото
-        photo_dict = dict(zip(name, link_list))
+    # Создаю словарь где keys = имя фото, values = ссылка на фото
+    def dict_name_link_photo (self):
+        photo_dict = dict(zip(self.photo_name(), self.photo_link()))
         return photo_dict
 
 mikhail = User(id = 552934290)
 
-print(mikhail.user_photos)
+print("Все фото пользователя", mikhail.user_photos())
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
+print("Названия фотографий пользователя", mikhail.photo_name())
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
+print("Ссылки на фотографии пользователя", mikhail.photo_link())
+print("_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_-_")
+print("Словарь названий и ссылок на фотографии", mikhail.dict_name_link_photo())
 
-457239027, 457239029, 457239031
